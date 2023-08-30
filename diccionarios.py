e2aaf14fed90363ca518532b4efb334f676dd98e
= <=>
2023-08-30 17:39:29 -0300

    #

--- diccionarios.py
+++ diccionarios.py
@@ -15,11 +15,33 @@ persona = {
 #persona['direccion'] = 'Lima'
 
 #lista = persona['gadget']
-print(persona.get('edad', 35))
+#print(persona.get('edad', 35))
 #persona.clear()
 #print(persona.keys())
 #print(persona.values())
 #print(persona.items())
+#print(persona.pop('edad'))
+#persona.update({'mascota': False})
+#print(persona)
+
+#for k, v in persona.items():
+   # print(k,v)
+    
+#tuplas
+
+tupla = (123,'Grover', 40, 1.75, 123)
+
+#print(tupla.count(123))
+#print(len(tupla))
+
+#set (conjuntos)
+
+conja = set('abracadabra')
+conjb = set('alacazam')
+print(conja)
+print(conjb)
+print(conjb-conja)
+print(conjb | conja)
 #persona.pop('edad')
 #persona.update({'mascota': False})
 #print(persona)
