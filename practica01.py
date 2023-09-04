244a4f8a127dfdf8ee6a37fd157e3a9712006466
= <=>
2023-09-04 18:32:37 -0300

    #

--- practica01.py
+++ practica01.py
@@ -1,16 +1,28 @@
+def descuentos(consumo):
+    if consumo > 0 and consumo <= 100:
+        descuento = consumo * 0.10
+    elif consumo > 100 and consumo <=200:
+        descuento = consumo * 0.20
+    elif consumo > 200:
+        descuento = consumo * 0.30
+    return descuento
+
+def impuesto(monto):
+    return monto * 0.15
+
 consumo = float(input('Ingrese el consumo del cliente: '))
 
-if consumo > 0 and consumo <= 100:
-    descuento = consumo * 0.10
-elif consumo > 100 and consumo <=200:
-    descuento = consumo * 0.20
-elif consumo > 200:
-    descuento = consumo * 0.30
+#if consumo > 0 and consumo <= 100:
+  #  descuento = consumo * 0.10
+#elif consumo > 100 and consumo <=200:
+   # descuento = consumo * 0.20
+#elif consumo > 200:
+  #  descuento = consumo * 0.30
 
 if(consumo > 0):
-    total = consumo - descuento
-    totalpago = total + total * 0.15
-    print('Descuento: ', descuento)
+    total = consumo - descuentos(consumo)
+    totalpago = total + impuesto(total)
+    print('Descuento: ', descuentos(consumo))
     print('Total: ', total)
     print('Total + Tax: ', totalpago)
 else:print('Consumo invalido')
