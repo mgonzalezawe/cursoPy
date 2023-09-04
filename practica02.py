45ae3eea13053454fe06aca68e7471c9940c4798
= <=>
2023-09-04 10:56:04 -0300

    #

---
+++ practica02.py
@@ -0,0 +1,18 @@
+import random
+
+pares = []
+impares = []
+numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+for n in numeros:
+    aleatorio = random.randint(1, 100)
+    result = n * aleatorio
+    
+    if result % 2 == 0:
+        pares.append(result)
+    else:
+        impares.append(result)
+    print(f'{n} x {aleatorio} = {result}')
+        
+print('lista de pares: ', pares)
+print('lista de impares: ', impares)
