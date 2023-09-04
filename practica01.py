57853ec203cd910d02970f77a50e1cd0b7d6e38a
= <=>
2023-09-04 10:41:25 -0300

    #

---
+++ practica01.py
@@ -0,0 +1,17 @@
+consumo = float(input('Ingrese el consumo del cliente: '))
+
+if consumo > 0 and consumo <= 100:
+    descuento = consumo * 0.10
+elif consumo > 100 and consumo <=200:
+    descuento = consumo * 0.20
+elif consumo > 200:
+    descuento = consumo * 0.30
+
+if(consumo > 0):
+    total = consumo - descuento
+    totalpago = total + total * 0.15
+    print('Descuento: ', descuento)
+    print('Total: ', total)
+    print('Total + Tax: ', totalpago)
+else:print('Consumo invalido')
+
