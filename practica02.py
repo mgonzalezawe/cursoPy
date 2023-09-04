bad70dde5908c6d21b2e877474a87a81fdff5668
= <=>
2023-09-04 18:40:11 -0300

    #

--- practica02.py
+++ practica02.py
@@ -4,15 +4,19 @@ pares = []
 impares = []
 numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 
+def agregarArreglo(num, n, aleatorio):
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+   
+    print(f'{n} x {aleatorio} = {num}')
+
+
 for n in numeros:
     aleatorio = random.randint(1, 100)
-    result = n * aleatorio
-    
-    if result % 2 == 0:
-        pares.append(result)
-    else:
-        impares.append(result)
-    print(f'{n} x {aleatorio} = {result}')
-        
+    agregarArreglo (n * aleatorio, n, aleatorio)
+      
+      
 print('lista de pares: ', pares)
 print('lista de impares: ', impares)
