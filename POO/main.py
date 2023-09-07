565ffb8bb694fca21544e8a710048e69bca54550
= <=>
2023-09-07 17:06:37 -0300

    #

---
+++ POO/main.py
@@ -0,0 +1,26 @@
+from poo import Persona
+from player import Player
+
+
+#Instancia de la clase
+"""per1 = Persona('Grover', 40, 99999) 
+per2 = Persona('Pablo', 29, 123456)
+
+per2.getDatos()
+print('='*10)
+per1.getDatos()"""
+
+player1 = Player('grover', 40)
+player2 = Player('Cindy', 30)
+print(player1)
+print(player2)
+
+
+"""print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('Hola'))
+print(type([]))
+print(type(()))
+print(type({}))"""
